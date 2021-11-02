bb4ef98a258001cb7cfd5e62c50afa564b591271
Aathil <aathilr7@gmail.com>
2021-11-02 13:40:04 +0530

    arrow function example

--- JavaScript Refresher/play.js
+++ JavaScript Refresher/play.js
@@ -4,7 +4,19 @@ let isOnline = true
 
 //username='John'  // this will raise an error since username is a const and cannot be changed
 function formatData(userName, userAge, userStatus) {
-    return( userName+' is aged '+userAge+'. Online:'+ userStatus)
+    return (userName + ' is aged ' + userAge + '. Online:' + userStatus)
 }
 
-console.log(formatData(username,age,isOnline))
+console.log(formatData(username, age, isOnline))
+
+const myfunc = function () { return "Inside myfunc()" }  //function definition without a name
+console.log(myfunc())
+const myfunc1 = myfunc
+console.log(myfunc1())
+
+
+//arrow function
+const foo = (arg1) => {
+    return( "Inside foo()...\narg1: "+arg1)
+}
+console.log(foo("johndoe"))
