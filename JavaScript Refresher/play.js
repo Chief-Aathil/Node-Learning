d5931fdfc8af98735803695eb199dd75dd18461d
Aathil <aathilr7@gmail.com>
2021-11-02 13:55:44 +0530

    More on arrow function syntax

--- JavaScript Refresher/play.js
+++ JavaScript Refresher/play.js
@@ -15,8 +15,17 @@ const myfunc1 = myfunc
 console.log(myfunc1())
 
 
-//arrow function
+//arrow functions
 const foo = (arg1) => {
-    return( "Inside foo()...\narg1: "+arg1)
+    return ("Inside foo()...\narg1: " + arg1)
 }
+
+const add = (a, b) => a + b  //no braces and return statement.since only one statement in function body
+const nextNum = (a) => a + 1
+const prevNum = a => a - 1   //no paranthesis for input arguments. since only one argument
+const printMsg = () => console.log('Inside printMsg()')
+console.log(nextNum(9))
+console.log(prevNum(10))
+console.log(add(1, 2))
 console.log(foo("johndoe"))
+printMsg()
