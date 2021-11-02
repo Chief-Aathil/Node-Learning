a8755a63151cf111b6e23e047a00e97ba162d3f4
Aathil <aathilr7@gmail.com>
2021-11-02 17:42:44 +0530

    More functions on arrays. spread operator. map from array

--- JavaScript Refresher/Arrays.js
+++ JavaScript Refresher/Arrays.js
@@ -8,3 +8,25 @@ for (let element of myArray) {
 for (let index in myArray) {
     console.log("myArray["+ index+"]:"+myArray[index])
 }
+
+//create map from array
+let myMap = myArray.map(ele=>'element:'+ele) 
+console.log(myMap)
+
+console.clear()
+const arr = [1, 2, 3, 4]
+console.log(arr)
+arr.push(5)     // this is allowed eventhough arr is a const.
+    /* this is because arr is a reference to the memory location where 
+        the actual array is stored
+    */
+console.log(arr)
+arr1 = arr      //copies reference
+arr2 = [arr]
+arr3 = [...arr] //spread operator . this copies arr to arr3
+arr1.push(6) // this change is reflected in arr also
+console.log('After changes:')
+console.log(arr)
+console.log(arr1)
+console.log(arr2)
+console.log(arr3)
