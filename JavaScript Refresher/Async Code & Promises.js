bd7018cd78c3d8afe3af7bfd7f5e530538ff2145
Aathil <aathilr7@gmail.com>
2021-11-03 15:57:00 +0530

    Async callback

--- JavaScript Refresher/Async Code & Promises.js
+++ JavaScript Refresher/Async Code & Promises.js
@@ -1,5 +1,22 @@
-//setTimeout executes the handler function once the timeout is expired
+//setTimeout executes the provided function once the timeout is expired
 setTimeout(() => {
     console.log('Timer is done')
-}, 100)
-console.log('here')  //this will be executed before the above time handler function.
+}, 20)
+console.log('here')  //this will be executed before the above provided function.
+
+
+
+const handler = (fn1, fn2) => {     //takes function object/reference
+    console.log("Inside handler()")
+    fn1()
+    setTimeout(fn2, 2000)       //calls the function after the given timeout
+}
+
+
+
+
+handler(() => {
+    console.log('Clearing console in 2 sec')
+    },
+    console.clear
+)
