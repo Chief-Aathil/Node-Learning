b00697910ef6850c23cda1372d729687b9238326
Aathil <aathilr7@gmail.com>
2021-11-03 11:37:58 +0530

    setTimeout()

---
+++ JavaScript Refresher/Async Code & Promises.js
@@ -0,0 +1,5 @@
+//setTimeout executes the handler function once the timeout is expired
+setTimeout(() => {
+    console.log('Timer is done')
+}, 100)
+console.log('here')  //this will be executed before the above time handler function.
