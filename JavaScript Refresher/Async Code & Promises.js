99292b87ee4c681523fa638ad40dd1ef080ee96e
Aathil <aathilr7@gmail.com>
2021-11-05 11:19:49 +0530

    Improved output clarity.

--- JavaScript Refresher/Async Code & Promises.js
+++ JavaScript Refresher/Async Code & Promises.js
@@ -1,22 +1,27 @@
 //setTimeout executes the provided function once the timeout is expired
-setTimeout(() => {
-    console.log('Timer is done')
-}, 20)
-console.log('here')  //this will be executed before the above provided function.
+// setTimeout(() => {
+//     console.log('Timer is done')
+// }, 20)
+// console.log('here')  //this will be executed before the above provided function.
 
 
 
 const handler = (fn1, fn2) => {     //takes function object/reference
     console.log("Inside handler()")
     fn1()
-    setTimeout(fn2, 2000)       //calls the function after the given timeout
+    setTimeout(fn2, 4000)       //calls the function after the given timeout
+}
+
+const f1 = () => {
+    console.log('inside f1()')
+}
+const f2 = () => {
+    console.log('inside f2()')
 }
 
 
 
-
-handler(() => {
-    console.log('Clearing console in 2 sec')
-    },
-    console.clear
-)
+// handler(() => { console.log('Clearing console in 4 sec') }
+//     , console.clear
+// )
+handler(f1, f2)
