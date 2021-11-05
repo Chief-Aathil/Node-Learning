c512031f8ce00999602fe346a9b1329a7a5e5e8d
Aathil <aathilr7@gmail.com>
2021-11-05 13:52:29 +0530

    Simple promise example

--- JavaScript Refresher/Async Code & Promises.js
+++ JavaScript Refresher/Async Code & Promises.js
@@ -4,24 +4,33 @@
 // }, 20)
 // console.log('here')  //this will be executed before the above provided function.
 
-
-
-const handler = (fn1, fn2, timeout) => {     //takes function object/reference
-    console.log("Inside handler(). timeout:", timeout)
-    fn1()
-    setTimeout(fn2, timeout)       //calls the function after the given timeout
-}
+const handler = (fn1, fn2, timeout) => {
+  //takes function object/reference
+  console.log("Inside handler(). timeout:", timeout);
+  fn1();
+  setTimeout(fn2, timeout); //calls the function after the given timeout
+};
 
 const f1 = () => {
-    console.log('inside f1()')
-}
+  console.log("inside f1()");
+};
 const f2 = () => {
-    console.log('inside f2()')
-}
-
-
+  console.log("inside f2()");
+};
 
 // handler(() => { console.log('Clearing console in 4 sec') }
 //     , console.clear
 // )
-handler(f1, f2, 2000)
+//handler(f1, f2, 2000);
+
+/*Promises*/
+const myPromise = new Promise((resolveFn, rejectFn) => {
+    resolveFn("Done!");
+});
+
+myPromise
+    .then(f1)
+    .then(f2)
+
+    
+myPromise.then(err => console.log(err));
