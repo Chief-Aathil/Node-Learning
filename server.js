86d467f0f6acd26326771d74d97ba5111225509a
Chief A <aathilr7@gmail.com>
2021-11-07 20:59:39 +0530

    Logging the request object

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ function reqListener(req, res) {
 }
 
 const server=http.createServer((req, res) => {      //storing the returned server object
-    console.log(req)
+    // console.log(req)
+    console.log(req.url, req.method, req.headers)
 });
 
 server.listen(3000);  //to start listening at the given port
