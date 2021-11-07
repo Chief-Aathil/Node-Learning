96dcd85f4a882d98abab6001b1e3eb7b17958078
Chief A <aathilr7@gmail.com>
2021-11-07 23:22:11 +0530

    Simple URL based logic

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ function reqListener(req, res) {
 const server=http.createServer((req, res) => {      //storing the returned server object
     // console.log(req)
     console.log(req.url, req.method, req.headers)
+    const url = req.url
+    if (url === '/') {
+        res.write('<html>')
+        res.write('<head><title>Enter Message</title></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit" >Send</button></input></form><body>')
+        res.write('</html>')
+        res.end();
+        return
+    }
     res.setHeader('Content-type', 'text/html')
     res.write('<html>')
     res.write('<head><title>My first page</title></head>')
