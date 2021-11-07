b7b453beb9e30ef3bb1831af390d1759d4e181fe
Chief A <aathilr7@gmail.com>
2021-11-07 21:11:30 +0530

    Simple response. Wrtiting to the response object directly.

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ function reqListener(req, res) {
 const server=http.createServer((req, res) => {      //storing the returned server object
     // console.log(req)
     console.log(req.url, req.method, req.headers)
+    res.setHeader('Content-type', 'text/html')
+    res.write('<html>')
+    res.write('<head><title>My first page</title></head>')
+    res.write('<body><h1>Hello from my Node.js server</h1></body>')
+    res.write('</html>')
+    res.end();
 });
 
 server.listen(3000);  //to start listening at the given port
