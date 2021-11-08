51edf524165d1259584d8e0bb8209aad7bddb898
Aathil <aathilr7@gmail.com>
2021-11-08 15:15:04 +0530

    Response  for '/users'

--- Assignments/1.Basics/routes.js
+++ Assignments/1.Basics/routes.js
@@ -1,6 +1,23 @@
 const reqHandler = (req, res) => {
   console.log("inside reqHandler()");
-  console.log(req.url);
+  const url = req.url;
+  if (url == "/") {
+    res.write("<head><title>First page</title></head>");
+    res.write("<body><h1>  Greetings !!!</h1>");
+    res.write("<form>");
+    res.write("</body></html>");
+    res.end();
+    return;
+  }
+  if (url == "/users") {
+    res.write("<head><title>User page</title></head>");
+    res.write("<body>");
+    res.write("<ul><li>User1</li><li>User2</li></ul>");
+    res.write("</body > ");
+    res.write("</html>");
+    res.end();
+    return;
+  }
 };
 
 exports.handler = reqHandler;
