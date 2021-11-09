25be06c3af5f13fc064096fe74fff8da9102b3b6
Aathil <aathilr7@gmail.com>
2021-11-09 11:27:19 +0530

    Adding middleware functions. working of next()

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-const http = require('http');     //import the http module
+const http = require("http"); //import the http module
 
-const express = require('express')
+const express = require("express");
 
 const app = express();
 
-console.log('---Starting Server---');
+console.log("---Starting Server---");
+
+app.use((req, res, next) => {
+    console.log("In the middleware");
+    next(); // to call the next use() in line
+});
+
+app.use((req, res, next) => {
+  console.log("In another middleware");
+});
+
+//the below use() will not be executed as the previous one doesn't call next()
+app.use((req, res, next) => {
+    console.log('In yet another middleware')
+});
+
+
 const server = http.createServer(app);
 
-server.listen(3000);  //to start listening at the given port
+server.listen(3000); //to start listening at the given port
