c9d861037246e98382a17d2fea4207437470dfcb
Aathil <aathilr7@gmail.com>
2021-11-09 09:59:19 +0530

    Logging server start

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const http = require('http');     //import the http module
 
 const routes = require('./routes');
 
+console.log('---Starting Server---');
 console.log(routes.someText);
-const server=http.createServer(routes.handler);
+const server = http.createServer(routes.handler);
 
 server.listen(3000);  //to start listening at the given port
