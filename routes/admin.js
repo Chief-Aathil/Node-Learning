91f1ed1846285e5925acbe745af08b11dfb3c14f
Aathil <aathilr7@gmail.com>
2021-11-09 16:05:28 +0530

    Sending file instead of write html.

--- routes/admin.js
+++ routes/admin.js
@@ -1,21 +1,14 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
 
-
 //get() uses exact match.
 //  /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  console.log("In first middleware");
-  console.log("url:", req.url);
-  res.send(
-    "<form action='/admin/add-product' method='POST'>" +
-      "<input type='text' name='title'>" +
-      "<button type='submit'>Add Product" +
-      "</button ></input ></form > "
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
-
 
 // /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -9,13 +11,19 @@ const shopRoutes = require("./routes/shop");
 console.log("---Starting Server---");
 
 app.use(bodyParser.urlencoded({ extended: false })); //to parse the request
+app.use((req, res, next) => {
+    const url = req.url;
+    const method = req.method;
+    console.log("-->Request", { url, method });
+    next();
+ });
 
 app.use("/admin",adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
     console.log(req.body);
-    res.status(404).send("<h1>Page not found</h1>");
+    res.status(404).sendFile(path.join(__dirname,"views","404.html"));
 });
 
 app.listen(3000);
