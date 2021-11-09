d62e0b0d9ef41fb0a18c11082b1482e674b52f0f
Aathil <aathilr7@gmail.com>
2021-11-09 14:48:46 +0530

    404 response created.

--- server.js
+++ server.js
@@ -8,9 +8,14 @@ const shopRoutes = require("./routes/shop");
 
 console.log("---Starting Server---");
 
-app.use(bodyParser.urlencoded({extended:false}));  //to parse the request
+app.use(bodyParser.urlencoded({ extended: false })); //to parse the request
 
 app.use(adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    console.log(req.body);
+    res.status(404).send("<h1>Page not found</h1>");
+});
 
 app.listen(3000);
